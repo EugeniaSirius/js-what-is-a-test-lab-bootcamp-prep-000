2abdebbf5e83b3854e300ae7598ee0ed9fb5afe8
Eugenia <eugenia.kaloshina@gmail.com>
2018-07-13 01:59:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,11 +2,6 @@
 var name = "Susan"
 var height = 40
 var message = ` is ${height} inces tall`
-   descibe ("height", ()=> {
-     it("less than 40",()=> {
-       expect (index.height).toBeLessThan(40)
-     })
-   })
-
+ 
 
 module.exports = {name, height, message}
